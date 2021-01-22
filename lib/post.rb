9262e470899160ff20d05a844387b51fad51b923
Eva <yi.zheng1709@gmail.com>
2021-01-22 15:56:03 -0500

    commit tree 7783e2e656beb514cfdbeb6c783f4afdd0f5621f

---
+++ lib/post.rb
@@ -0,0 +1,8 @@
+class Post 
+    attr_accessor :author, :title 
+
+    def inititalize(title, author)
+        @title = title 
+        Author.new = author 
+    end 
+end 
---
+++ lib/song.rb
@@ -0,0 +1,8 @@
+class Song 
+    attr_accessor :title, :artist 
+
+    def inititalize(title, artist)
+        @title = title 
+        Artist.new = artist 
+    end 
+end 
